25ce5f0ee8ceb755c9191b379ae104c364083445
James Bentham <jbentham@f0r4s22.codam.nl>
2026-08-17 16:59:52 +0200

    Added models.py file for storing dataclasses

---
+++ src/models.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+
+@dataclass
+class Hub:
+    name: str
+    x: int
+    y: int
+    metadata: dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class Connection:
+    start: Hub
+    end: Hub
+    metadata: dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class Network:
+    nb_drones: int
+    start_hub: Hub
+    end_hub: Hub
+    hubs: list[Hub]
+    connections: list[Connection]
+
+
+@dataclass
+class Drone:
+    id: int
+    current_hub: Hub
+
+
+@dataclass
+class Simulation:
+    network: Network
+    drones: list[Drone]
+
