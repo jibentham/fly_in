21f7ec439544abd49be8eedb5e620e946d0b5d04
James Bentham <jbentham@f0r4s22.codam.nl>
2026-08-17 15:46:16 +0200

    changed type hinting for 'data' attribute. Added sorted() method to directory/file processing for deterministic output

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 from parser import parse_config
+from typing import Any
 
 
 def main() -> None:
     source_directory: Path = Path(__file__).resolve().parent
     map_directory: Path = source_directory.parent/"maps"
 
-    for directory in map_directory.iterdir():
+    for directory in sorted(map_directory.iterdir()):
         if not directory.is_dir():
             continue
-        for file in directory.glob("*.txt"):
-            data: dict[str, list[str]] = parse_config(file)
+        for file in sorted(directory.glob("*.txt")):
+            data: dict[str, Any] = parse_config(file)
             print("\n------------------------------------------------")
             print(f"\n{file.name}\n")
             print(data)
